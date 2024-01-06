4edcf1c264b08b5ab1d5a17cd8638e349b392362
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-01-06 16:02:11 -0300

    feat: Instala e configura o ValidationPipe

---
+++ src/user/user.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UserController {
+  @Post()
+  async create(@Body() body) {
+    return { body };
+  }
+  @Get()
+  async read() {
+    return { users: [] };
+  }
+
+  @Get(':id')
+  async show(@Param() params) {
+    return { user: {}, params };
+  }
+
+  @Put(':id')
+  async update(@Body() body, @Param() params) {
+    return {
+      method: 'put',
+      body,
+      params,
+    };
+  }
+
+  @Patch(':id')
+  async updateParcial(@Body() body, @Param() params) {
+    return {
+      method: 'patch',
+      body,
+      params,
+    };
+  }
+
+  @Delete(':id')
+  async delete(@Param() params) {
+    return {
+      params,
+    };
+  }
+}
