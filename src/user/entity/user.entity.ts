d524f003df1a0d0463a200064d27a04908540087
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-01-13 20:02:14 -0300

    feat: Constroi a class UserEntity

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,48 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn({
+    unsigned: true,
+  })
+  id: number;
+
+  @Column({
+    length: 63,
+  })
+  name: string;
+
+  @Column({
+    length: 127,
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    length: 127,
+  })
+  password: string;
+
+  @Column({
+    type: 'date',
+    nullable: true,
+  })
+  birthAt: string;
+
+  @CreateDateColumn()
+  createAt: string;
+
+  @UpdateDateColumn()
+  updateAt: string;
+
+  @Column({
+    enum: [1, 2],
+  })
+  role: number;
+}
